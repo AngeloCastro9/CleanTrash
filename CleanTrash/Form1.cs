2de09ebe8bb4e3a0bad1fbe9e1ec27fb4652b0b9
Ângelo Castro <42939130+AngeloCastro9@users.noreply.github.com>
2020-03-27 15:10:21 -0300

    Begin

---
+++ CleanTrash/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CleanTrash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            deleteManager dm = new deleteManager();
+            if (cbBak.Checked)
+                dm.delete("*.txt", folderPath.Text);
+            if (cbFbk.Checked)
+                dm.delete("*.bmp", folderPath.Text);
+            if (cbFdb.Checked)
+                dm.delete("*.rar", folderPath.Text);
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            folderPath.Text = dialog.SelectedPath;
+        }
+    }
+}
---
+++ CleanTrash/deleteManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CleanTrash
+{
+    class deleteManager
+    {
+        public void delete(string extension, string folderPath)
+        {
+
+            if (folderPath != "")
+            {
+                var pastaOrigem = folderPath;
+                var arquivos = new DirectoryInfo(pastaOrigem).GetFiles(extension, SearchOption.AllDirectories);
+                foreach (var arquivo in arquivos)
+                {
+                    var nomeArquivo = Path.GetFullPath(arquivo.FullName);
+                    try
+                    {
+                        File.Delete(nomeArquivo);
+
+                    }
+                    catch (Exception err)
+                    {
+                        MessageBox.Show(err.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid path");
+            }
+        }
+    }
+}
